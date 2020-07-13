4e2dbfbaa45c1ace9135d98180d6283f05374006
guoyuMao <gy_mao@126.com>
2020-07-14 07:45:52 +0800

    test two

--- src/main/java/gy/mao/test_git_operator/GitTest.java
+++ src/main/java/gy/mao/test_git_operator/GitTest.java
@@ -1,4 +1,10 @@
 package gy.mao.test_git_operator;
 
 public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("测试git相关操作");
+        System.out.println("测试git相关操作");
+        System.out.println("测试git相关操作");
+        System.out.println("测试git相关操作");
+    }
 }
